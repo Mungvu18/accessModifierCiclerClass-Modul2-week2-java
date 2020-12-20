929b443f831df1bfe0834b7974d54fac4de7e176
MungC2010G2 <zhaiduong26z@gmail.com>
2020-12-20 17:26:02 +0700

    updater file class circle

---
+++ src/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+import Cirlce.Circle;
+
+public class Test {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println("Bán kình hình tròn là: " + circle.getRadius());
+        System.out.println("diện tích hình tròn là: " + circle.getArea());
+    }
+}
